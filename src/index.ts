31498c29795842de0dd56a97694f2b1aaf047309
cassieview <cassie.siljander@microsoft.com>
2019-03-29 13:16:23 -0500

    added vr helper and second sphere, updated name

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,30 @@ var canvas: any = document.getElementById("renderCanvas");
 var engine: Engine = new Engine(canvas, true);
 
 function createScene(): Scene {
+    // Create scene
     var scene: Scene = new Scene(engine);
 
+    // Create simple sphere
+    var sphere = BABYLON.Mesh.CreateIcoSphere("sphere", { radius: 0.2, flat: true, subdivisions: 1 }, scene);
+    sphere.position.y = 3;
+    sphere.material = new BABYLON.StandardMaterial("sphere material", scene)
+
+    // Lights and camera
     var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
     camera.attachControl(canvas, true);
-
     var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
 
-    var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
-    addLabelToMesh(sphere);
+    // Create sphere and add label
+    var sphere1: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
+    sphere1.position.y = 5;
+    sphere1.material = new BABYLON.StandardMaterial("sphere material", scene)
+    addLabelToMesh(sphere1);
+
+
+    // Enable VR
+    var vrHelper = scene.createDefaultVRExperience();
+    vrHelper.enableInteractions();
+
     return scene;
 }
 
