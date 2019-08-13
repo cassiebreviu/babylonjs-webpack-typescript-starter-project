5da9dd25115980d3e94c5d738b446a7cfdbe15e5
cassieview <cassie.siljander@microsoft.com>
2019-08-12 19:32:08 -0500

    update packages and index file

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,10 @@ var engine: Engine = new Engine(canvas, true);
 function createScene(): Scene {
     // Create scene
     var scene: Scene = new Scene(engine);
-
+    scene.clearColor = BABYLON.Color4.FromColor3(BABYLON.Color3.Black());
+    
     // Create camera
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(6, 5, -10), scene);
+    var camera = new BABYLON.FreeCamera("Camera", new BABYLON.Vector3(0, 0, -10), scene);
     camera.setTarget(BABYLON.Vector3.Zero());
     camera.attachControl(canvas, true);
 
@@ -17,8 +18,12 @@ function createScene(): Scene {
     var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
     sphere.position.y = 1;
 
-    // Create
-    var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+
+    // Create Ground
+    var ground = BABYLON.Mesh.CreatePlane("ground", 25.0, scene);
+    ground.position = new BABYLON.Vector3(0, -5, 0);
+    ground.rotation = new BABYLON.Vector3(Math.PI / 2, 0, 0);
+
 
     // Enable VR
     //var vrHelper = scene.createDefaultVRExperience();
