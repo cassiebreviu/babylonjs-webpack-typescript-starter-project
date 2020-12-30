83a2f8dd7794e2c53a4fb4a033ee75b513c94f1a
Eric Jorgensen <ericjorg@thejcrew.net>
2020-12-30 12:06:43 -0800

    Typescript cleanup, updating packages, structure cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,61 @@
 import * as BABYLON from "babylonjs";
-// Get the canvas DOM element
-var canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
-// Load the 3D engine
-var engine = null;
-var scene = null;
-var sceneToRender = null;
-var createDefaultEngine = function () {
-  return new BABYLON.Engine(canvas, true, {
-    preserveDrawingBuffer: true,
-    stencil: true,
-  });
-};
+import { AbstractMesh } from "babylonjs";
 
+// Some global handles for graphics
+const theCanvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
+const engine = new BABYLON.Engine(
+    theCanvas, 
+    true, 
+    {
+        preserveDrawingBuffer: true,
+        stencil: true,
+    });
+
+if (!engine) throw "Unable to create an engine!";
+
+// -------------------------------------------------------------------------------
+// Create scene
+// -------------------------------------------------------------------------------
 var createScene = async function () {
-  var scene = new BABYLON.Scene(engine);
-  var camera = new BABYLON.FreeCamera(
-    "camera1",
-    new BABYLON.Vector3(0, 5, -10),
-    scene
-  );
-  camera.setTarget(BABYLON.Vector3.Zero());
-  camera.attachControl(canvas, true);
-  var light = new BABYLON.HemisphericLight(
-    "light1",
-    new BABYLON.Vector3(0, 1, 0),
-    scene
-  );
-  light.intensity = 0.7;
-  var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-  sphere.position.y = 1;
+    const scene = new BABYLON.Scene(engine);
+    var camera = new BABYLON.FreeCamera(
+        "camera1",
+        new BABYLON.Vector3(0, 5, -10),
+        scene
+    );
+    camera.setTarget(BABYLON.Vector3.Zero());
+    camera.attachControl(theCanvas, true);
 
-  const env = scene.createDefaultEnvironment();
+    // Add a light
+    var light = new BABYLON.HemisphericLight(
+        "light1",
+        new BABYLON.Vector3(0, 1, 0),
+        scene
+    );
+    light.intensity = 0.7;
 
-  const xr = await scene.createDefaultXRExperienceAsync({
-    floorMeshes: [env.ground],
-  });
+    // Add an object
+    var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+    sphere.position.y = 1;
+       
+    const env = scene.createDefaultEnvironment();
+    if(!env) return null;
 
-  return scene;
+    await scene.createDefaultXRExperienceAsync({
+        floorMeshes: [env.ground as AbstractMesh],
+    });
+
+    return scene;
 };
 
-var engine;
-try {
-  engine = createDefaultEngine();
-} catch (e) {
-  console.log(
-    "the available createEngine function failed. Creating the default engine instead"
-  );
-  engine = createDefaultEngine();
-}
-if (!engine) throw "engine should not be null.";
-scene = createScene();
-scene.then((returnedScene) => {
-  sceneToRender = returnedScene;
-});
+// -------------------------------------------------------------------------------
+// Run the game
+// -------------------------------------------------------------------------------
+(async function () {
+    const sceneToRender = await createScene();
+    if(!sceneToRender) throw "Unable to create a scene!";
 
-engine.runRenderLoop(function () {
-  if (sceneToRender) {
-    sceneToRender.render();
-  }
-});
+    window.addEventListener("resize", () => { engine.resize(); });
+    engine.runRenderLoop(() => sceneToRender.render())
+})()
 
-// Resize
-window.addEventListener("resize", function () {
-  engine.resize();
-});
